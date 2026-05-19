114056a7b6f57628715c2365d1dc961ea20350ea
Jerimiah Ham <jham@scorpius.lan>
2026-05-19 11:22:58 -0700

    chore(deps): bump swift-libp2p pin → afff31d (Responder/Cache/Sessions guards)

--- Package.swift
+++ Package.swift
@@ -41,7 +41,7 @@ let package = Package(
         // change in jerimiah797/swift-libp2p-kad-dht).
         .package(
             url: "https://github.com/jerimiah797/swift-libp2p.git",
-            revision: "375393435e5b81bf253d554396f065c814be2dd6"
+            revision: "afff31ddd525222e30550ca0e9988948e1684ed8"
         ),
 
         // NIO Test Utils
