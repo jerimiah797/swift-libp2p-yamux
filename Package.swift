93971a296d661e503486aedb315ee0bdcfbe4341
Jerimiah Ham <jerimiah797@gmail.com>
2026-05-19 10:57:00 -0700

    chore(deps): bump swift-libp2p pin → 3753934 (stub channel handler)
    Picks up the fix to NoOpTransportUpgrader.negotiate(_) so it
    returns a real ChannelHandler. Mechanical revision bump.

--- Package.swift
+++ Package.swift
@@ -41,7 +41,7 @@ let package = Package(
         // change in jerimiah797/swift-libp2p-kad-dht).
         .package(
             url: "https://github.com/jerimiah797/swift-libp2p.git",
-            revision: "ec452d1164a9a4b94118198c067aacf35a4a937b"
+            revision: "375393435e5b81bf253d554396f065c814be2dd6"
         ),
 
         // NIO Test Utils
